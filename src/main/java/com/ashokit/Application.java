44c25771323cf43bfd4d31ad01e427278ddc39f7
unknown <santoshbaranjalekar@gmail.com>
2022-09-23 19:19:04 +0530

    Main Class Modified

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -2,12 +2,14 @@ package com.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext context=SpringApplication.run(Application.class, args);
+		System.out.println("Application Context");
 	}
 
 }
